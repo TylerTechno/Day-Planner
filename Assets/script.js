1a30872c2b445359002616f0a988cdb3fe77fa83
Tyler Everett <tylertechnotrade@gmail.com>
2022-01-01 16:33:34 -0800

    still creating js

--- Assets/script.js
+++ Assets/script.js
@@ -6,3 +6,33 @@ var eventInfoCache = {};
 var eventInfo = {
     timeStart: 0,
     content: "",
+
+    save: function() {
+        eventInfoCache[this.timeStart] = this.content;
+        localStorage.setItem('eventInfo', JSON.stringify(eventInfoCache))
+  },
+  load: function() {
+      eventInfoCache = JSON.parse(localStorage.getItem('eventInfo'))
+
+      if (eventInfoCache != null) {
+          for (const [key, value] of Object.entries(eventInfoCache)) {
+              var thE1 = $(`th:contains('${key}')`)
+              thE1.siblings('col-event').text(value)
+
+          }
+      } else {
+          eventInfoCache = {}
+      }
+  }
+}
+
+var time = moment().format('dddd, mmmm do yyyyy, hh:mm:ss a');
+$('#currentDay').text(time);
+
+setInterval(function() {
+    time = moment().format('dddd, mmmm do yyyyy, hh:mm:ss a');
+    $('#currentDay').text(time);
+}, 1000);
+
+const tableE1 = $('<table>').addClass('table');
+const tBodyE1 = $('<tbody>');
