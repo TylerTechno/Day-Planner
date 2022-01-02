a562369d474abfa03ef60372022088698650f22a
Tyler Everett <tylertechnotrade@gmail.com>
2022-01-01 18:48:11 -0800

    fixed error in js

--- Assets/script.js
+++ Assets/script.js
@@ -11,13 +11,14 @@ var eventInfo = {
         eventInfoCache[this.timeStart] = this.content;
         localStorage.setItem('eventInfo', JSON.stringify(eventInfoCache))
   },
+
   load: function() {
       eventInfoCache = JSON.parse(localStorage.getItem('eventInfo'))
 
       if (eventInfoCache != null) {
           for (const [key, value] of Object.entries(eventInfoCache)) {
               var thE1 = $(`th:contains('${key}')`)
-              thE1.siblings('col-event').text(value)
+              thE1.siblings('.col-event').text(value)
 
           }
       } else {
@@ -26,11 +27,11 @@ var eventInfo = {
   }
 }
 
-var time = moment().format('dddd, mmmm do yyyyy, hh:mm:ss a');
+var time = moment().format('dddd, MMMM Do YYYY, hh:mm:ss a');
 $('#currentDay').text(time);
 
 setInterval(function() {
-    time = moment().format('dddd, mmmm do yyyyy, hh:mm:ss a');
+    time = moment().format('dddd, MMMM Do YYYY, hh:mm:ss a');
     $('#currentDay').text(time);
 }, 1000);
 
@@ -38,7 +39,7 @@ const tableE1 = $('<table>').addClass('table');
 const tBodyE1 = $('<tbody>');
 
 for (hour = 0 + hourStart; hour < hoursInDay + hourStart; hour++) {
-    var cellHour = moment().subtract(moment().format('hh') - hour, 'h')
+    var cellHour = moment().subtract(moment().format('HH') - hour, 'H')
     var tRowE1 = $('<tr>').addClass('time-block');
     var tHeadEl = $('<th>').attr('scope', 'row').addClass('hour align-middle').text(cellHour.format('hh:00 A'));
     var tRowEventEl = $('<td>').addClass('col-event text-break text-wrap text-left data-editable').attr('contenteditable', true);
@@ -65,7 +66,7 @@ for (hour = 0 + hourStart; hour < hoursInDay + hourStart; hour++) {
 tableE1.append(tBodyE1);
 containerEl.append(tableE1);
 
-$('table').on('click', 'saveBtn',function(event) {
+$('table').on('click', '.saveBtn',function(event) {
     eventInfo.timeStart = $(event.target).siblings('.hour').text();
     eventInfo.content = $(event.target).siblings('.col-event').text();
     eventInfo.save();
